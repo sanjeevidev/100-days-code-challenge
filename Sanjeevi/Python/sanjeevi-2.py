f35db5a80f8a399307e3c08b0ad807e8272e941c
Sanjeevi Kumar V <sanjeevikumar116@gmail.com>
2023-05-02 22:41:04 +0530

    Day-2 🚀

---
+++ Sanjeevi/Python/sanjeevi-2.py
@@ -0,0 +1,14 @@
+# Arithmetic Operations
+a = int(input("Enter the value of a :"))
+b = int(input("Enter the value of b :"))
+
+# Sum
+print(f"The sum of a and b is {a+b}")
+# Difference
+print(f"The sum of a and b is {abs(a-b)}")
+# Product
+print(f"The sum of a and b is {a*b}")
+# Division
+print(f"The sum of a and b is {a/b}")
+# Modulus
+print(f"The sum of a and b is {a%b}")
