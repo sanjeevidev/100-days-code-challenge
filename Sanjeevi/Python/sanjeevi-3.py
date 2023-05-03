c8500ae4e0d03f8657860fef24e92dc8ec013a12
Sanjeevi Kumar V <sanjeevikumar116@gmail.com>
2023-05-03 19:48:07 +0530

    Day-3 🚀

---
+++ Sanjeevi/Python/sanjeevi-3.py
@@ -0,0 +1,21 @@
+# String Operations
+msg = "Python Programming"
+
+#Indexing a String
+print(msg[0])
+#Negative Indexing
+print(msg[-2]) 
+#Slicing a String
+print(msg[0:3])
+#Striding a String
+print(msg[2:5:2])
+#Concantenation of Strings
+print("I love",msg)
+#Uppercase - Changes the value to Uppercase
+print(msg.upper())
+#Lowercase - Changes the value to Lowercase
+print(msg.lower())
+#Swapcase - Swaps the Lower and Uppercase
+print(msg.swapcase())
+#CaseFold - Returns the lowercase of the data
+print(msg.casefold())
